af77b82796e78ad0fbe799a9728321b026638840
Adam Frey <adam@adamfrey.me>
2017-01-09 20:46:11 -0500

    Update indentation

--- src/afrey/boot_asset_fingerprint.clj
+++ src/afrey/boot_asset_fingerprint.clj
@@ -22,11 +22,10 @@
   Why Should I Care\" for a detailed explanation of why you want to do this.
   (http://guides.rubyonrails.org/asset_pipeline.html#what-is-fingerprinting-and-why-should-i-care-questionmark) "
   [s skip            bool  "Skips file fingerprinting and replaces each asset url with bare"
-   e extensions  EXT  [str] "Add a file extension to indicate the files to process for asset references."
-   _ asset-host HOST str   "Host to prefix all asset urls with"
-   ]
-  (let [prev        (atom nil)
-        tmp-dir     (core/tmp-dir!)
+   e extensions EXT  [str] "Add a file extension to indicate the files to process for asset references."
+   _ asset-host HOST str   "Host to prefix all asset urls with"]
+  (let [prev       (atom nil)
+        tmp-dir    (core/tmp-dir!)
         extensions (or extensions [".html"])]
     (core/with-pre-wrap fileset
       (core/empty-dir! tmp-dir)
@@ -37,18 +36,18 @@
                           (core/output-files fileset))]
         (reset! prev fileset)
         (doseq [file files
-                :let [path (:path file)
-                      input-path (-> file core/tmp-file .getPath)
+                :let [path        (:path file)
+                      input-path  (-> file core/tmp-file .getPath)
                       output-path (-> (io/file tmp-dir path) .getPath)
-                      input-root (path->parent-path path)]]
+                      input-root  (path->parent-path path)]]
           (do
             (util/info "Fingerprinting %s...\n" path)
             (impl/fingerprint
-             {:input-path  input-path
-              :input-root  input-root
-              :output-path output-path
-              :fingerprint? (not skip)
-              :asset-host asset-host
-              :file-hashes file-hashes})))
+              {:input-path   input-path
+               :input-root   input-root
+               :output-path  output-path
+               :fingerprint? (not skip)
+               :asset-host   asset-host
+               :file-hashes  file-hashes})))
 
         (-> fileset (core/add-resource tmp-dir) core/commit!)))))
